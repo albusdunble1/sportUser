9e40e54eac46e00c225f62b0f2dcb3460d4aafe8
Ng Wei Sheng <weisheng_rules@hotmail.com>
2018-05-20 03:41:52 +0800

    carnival commit user

--- src/pages/reserve-date/reserve-date.ts
+++ src/pages/reserve-date/reserve-date.ts
@@ -2,7 +2,7 @@ import { CommonProvider } from './../../providers/common/common';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { DateTime } from './../../app/models/datetime.interface';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, OnChanges } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ReserveItemPage } from '../reserve-item/reserve-item';
 import { Subject } from 'rxjs/Subject';
@@ -19,6 +19,7 @@ import moment from 'moment';
 export class ReserveDatePage implements OnInit, OnDestroy{
   timeForm: FormGroup;
   myDate= new Date().toString();
+  dateCheckCopy:string;
   reservation$;
   category$;
   reservationArray=[];
@@ -43,22 +44,28 @@ export class ReserveDatePage implements OnInit, OnDestroy{
   minDate: string = new Date().toISOString();
   dateMax = moment();
   maxDate:string;
+
+  
   
 
   categorySub: Subscription;
   reservationSub: Subscription;
   querySubscription: Subscription;
   reservationTimeSub:  Subscription;
+  proceedDisabled= true;
 
   reservationTimes =["1pm-2pm", "2pm-3pm", "3pm-4pm", "4pm-5pm", "5pm-6pm"];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private afDB: AngularFireDatabase, private common: CommonProvider) {
+    this.dateCheckCopy = this.myDate;
     this.dateMax.add(1, 'months'); // get max date that user can book
     this.maxDate= this.dateMax.toDate().toISOString();
-
+    console.log(this.myDate);
     this.userId= this.common.getUser();
     this.matricsNo= this.common.getUserEmail().substring(0,7);
     
+    
+
 
     //fetching category list
     this.category$ = afDB.list("category").snapshotChanges()
@@ -108,6 +115,18 @@ export class ReserveDatePage implements OnInit, OnDestroy{
   ngOnInit(){
     this.initializeForm();
   }
+
+
+  onChangeDate(){
+    // this.proceedDisabled = false;
+    if(this.myDate === this.dateCheckCopy){
+      this.proceedDisabled = true;
+    }else{
+      this.proceedDisabled = false;
+    }
+    console.log("change date called");
+  }
+
   
 
   private initializeForm(){
@@ -130,6 +149,7 @@ export class ReserveDatePage implements OnInit, OnDestroy{
   // }
 
   onSubmit(){
+    console.log("submit called");
     this.dateTime.date= this.myDate;
     this.dateTime.time= this.timeForm.value.time;
     this.reservationTimeRef$= this.afDB.list('/reservationTimes/'+ this.dateTime.time + '/'+ this.dateTime.date);
@@ -175,7 +195,53 @@ export class ReserveDatePage implements OnInit, OnDestroy{
          }
        )
 
-       this.querySubscription=this.reservationQuery.subscribe(      ///problemmmmm (solved for now)
+
+
+//TESTING PROMISE (ORIGINAL CODE)
+
+
+      //  this.querySubscription=this.reservationQuery.subscribe(      ///problemmmmm (solved for now)
+      //   (data) => {
+      //     this.reservationComparisonArray= data;
+      //     // console.log(this.reservationComparisonArray);
+
+      //     let counter =0;
+      //     this.reservationComparisonArray.forEach(
+      //       (check) => {
+      //           if(check.key == this.dateTime.date){
+      //             counter++;
+      //           }
+      //         }
+      //     )
+      //     if(counter> 0){
+      //       // console.log('found');
+            
+      //     }else{
+      //       this.reservationTimeRef$.push({category: this.categoryArray});
+            
+      //     }
+      //   }
+      // )
+       
+      // this.navCtrl.push(ReserveItemPage, {dateTime:this.dateTime});
+     
+//TESTING PROMISE (ORIGINAL CODE)
+
+      // this.reservationFilteredArray= this.reservationComparisonArray.filter(
+      //    (data) => {
+      //      data.key == this.dateTime.date;
+      //      }
+      //     )
+
+      //     if(this.reservationFilteredArray.length>0){
+      //           console.log('already exist');
+      //         }else{
+                // this.reservationTimeRef$.push({category: this.categoryArray});
+      //         }
+       
+let testPromise = new Promise(
+  (resolve)=> {
+    this.querySubscription=this.reservationQuery.subscribe(      ///problemmmmm (solved for now)
         (data) => {
           this.reservationComparisonArray= data;
           // console.log(this.reservationComparisonArray);
@@ -190,30 +256,27 @@ export class ReserveDatePage implements OnInit, OnDestroy{
           )
           if(counter> 0){
             // console.log('found');
-            
+            resolve("nothing");
           }else{
             this.reservationTimeRef$.push({category: this.categoryArray});
-            
+            resolve("nothing");
           }
         }
       )
-       
-      this.navCtrl.push(ReserveItemPage, {dateTime:this.dateTime});
-     
-      // this.reservationFilteredArray= this.reservationComparisonArray.filter(
-      //    (data) => {
-      //      data.key == this.dateTime.date;
-      //      }
-      //     )
 
-      //     if(this.reservationFilteredArray.length>0){
-      //           console.log('already exist');
-      //         }else{
-                // this.reservationTimeRef$.push({category: this.categoryArray});
-      //         }
+      
+  }
+)  
+
+testPromise.then(
+  (data)=>{
+    console.log("promise successful!"+data);
+    this.navCtrl.push(ReserveItemPage, {dateTime:this.dateTime});
+  }
+);
+ 
        
       
-
      
 
 
